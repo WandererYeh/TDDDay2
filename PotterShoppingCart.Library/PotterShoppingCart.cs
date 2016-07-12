df01cccc97ae08cbd13cf6775f3de74b21d2ec30
WandererYeh <wandereryeah@hotmail.com>
2016-07-12 13:44:51 +0800

    綠燈 - CalculateOrderSumTest_Buy_Only_One_Book1_Should_Be_100
    實作Test / Production Code，通過Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元

--- PotterShoppingCart.Library/PotterShoppingCart.cs
+++ PotterShoppingCart.Library/PotterShoppingCart.cs
@@ -8,10 +8,27 @@ namespace PotterShoppingCart.Library
 {
     public class PotterShoppingCart
     {
-        public double CalculateOrderSum() {
-            double sum = 0;
+        public int CalculateOrderSum(List<Order> orders) {
+            int sum = 0;
+
+            var orderList = (
+                from order in orders
+                group order by new { order.Name, order.Price } into g
+                select new Order { Name = g.Key.Name, Price = g.Key.Price, Qty = g.Sum(order => order.Qty) }
+            ).ToList();
+
+            foreach (var v in orderList)
+            {
+                sum += v.Price;
+            }
 
             return sum;
         }
+    }
+
+    public class Order {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Qty { get; set; }
     }
 }
--- PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -12,9 +12,13 @@ namespace PotterShoppingCart.Tests
     {
         [TestMethod()]
         public void CalculateOrderSumTest_Buy_Only_One_Book1_Should_Be_100() {
+            var orders = new List<Order> {
+                new Order { Name = "Book1", Price = 100, Qty = 1 }
+            };
+
             var target = new PotterShoppingCart.Library.PotterShoppingCart();
             var expected = 100;
-            var actual = target.CalculateOrderSum();
+            var actual = target.CalculateOrderSum(orders);
             Assert.AreEqual(expected, actual);
         }
     }
