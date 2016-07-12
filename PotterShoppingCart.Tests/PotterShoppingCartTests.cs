ba982ef256660995d25a546a4265fb6c3cb0dc63
WandererYeh <wandereryeah@hotmail.com>
2016-07-12 12:06:12 +0800

    紅燈 - CalculateOrderSumTest_Buy_Only_One_Book1_Should_Be_100
    CalculateOrderSumTest_Buy_Only_One_Book1_Should_Be_100 -
    第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart.Library;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod()]
+        public void CalculateOrderSumTest_Buy_Only_One_Book1_Should_Be_100() {
+            var target = new PotterShoppingCart.Library.PotterShoppingCart();
+            var expected = 100;
+            var actual = target.CalculateOrderSum();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
