b12ea800d32edd12ae963fd8386034b2028d1c31
WandererYeh <wandereryeah@hotmail.com>
2016-07-12 14:33:09 +0800

    綠燈 - CountOrderSumTest_Buy_One_Book1_Two_Book2_Two_Book3_Should_Be_460
    新增測試Scenario: 第一集買了一本，第二三集各買了兩本，價格應為100*3*0.9 + 100*2*0.95 = 460，通過所有測試

--- PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -96,5 +96,21 @@ namespace PotterShoppingCart.Tests
             var actual = target.CalculateOrderSum(orders);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CountOrderSumTest_Buy_One_Book1_Two_Book2_Two_Book3_Should_Be_460() {
+            var orders = new List<Order> {
+                new Order { Name = "Book1", Price = 100, Qty = 1 },
+                new Order { Name = "Book2", Price = 100, Qty = 1 },
+                new Order { Name = "Book2", Price = 100, Qty = 1 },
+                new Order { Name = "Book3", Price = 100, Qty = 1 },
+                new Order { Name = "Book3", Price = 100, Qty = 1 }
+            };
+
+            var target = new PotterShoppingCart.Library.PotterShoppingCart();
+            var expected = 460;
+            var actual = target.CalculateOrderSum(orders);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
